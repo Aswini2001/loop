3004a2b0e5fea025bd9ec9f6856e7cf29785c5fb
Aswini2001 <64783778+Aswini2001@users.noreply.github.com>
2020-05-10 17:39:56 +0530

    created as pattern loop.c
    the output will be a diamond pattern

---
+++ pattern loop.c
@@ -0,0 +1,31 @@
+ #include<stdio.h>
+int main()
+{
+	int i,j,m,n;
+	printf("enter the number of rows=");
+	scanf("%d",&n);
+	for(i=0;i<=n;++i){
+		for(j=0;j<=n-i;++j){
+			printf(" ");
+		}
+		for(m=1;m<=i;m++){
+			printf("10");
+		}
+       
+     printf("\n");		
+	}
+	 for(i=0;i<=n;++i)
+		{
+			for(j=0;j<=i;++j)
+			{
+				printf(" ");
+			}
+			for(m=1;m<=n-i;m++)
+			{ 
+			printf("10");
+			}
+			printf("\n");
+				}		
+		
+	return 0;
+}
